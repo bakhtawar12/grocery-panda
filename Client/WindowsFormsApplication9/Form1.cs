f1ab913fd8860d5790757f970e0e01037a5494d1
bakhtawar12 <38508929+bakhtawar12@users.noreply.github.com>
2018-04-19 15:39:09 +0500

    Add at Client

---
+++ Client/WindowsFormsApplication9/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            localhost.Service1 v = new localhost.Service1();
+            v.register(textBox1.Text, textBox2.Text, comboBox1.Text, textBox3.Text);
+            MessageBox.Show("Succesfully Register");
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Form2 frm = new Form2();
+            frm.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ Client/WindowsFormsApplication9/Form2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication9
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool isvalidresult;
+            bool isvalidpassed;
+            localhost.Service1 d = new localhost.Service1();
+            d.isvalid(textBox1.Text, textBox2.Text, out isvalidresult, out isvalidpassed);
+            if (isvalidresult)
+            {
+                MessageBox.Show(" Valid user");
+            }
+            else
+            {
+                MessageBox.Show("invalid user");
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Form1 frm = new Form1();
+            frm.Show();
+            this.Hide();
+
+        }
+    }
+}
