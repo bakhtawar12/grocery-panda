8a478aa8269ced4aceebfe7ca067a2f496230417
bakhtawar12 <38508929+bakhtawar12@users.noreply.github.com>
2018-04-24 23:27:33 +0500

    ererer

--- Client/WindowsFormsApplication9/Form2.Designer.cs
+++ Client/WindowsFormsApplication9/Form2.Designer.cs
@@ -81,7 +81,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(91, 198);
+            this.button1.Location = new System.Drawing.Point(56, 208);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 23);
             this.button1.TabIndex = 5;
